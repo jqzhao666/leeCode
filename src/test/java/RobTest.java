a2882b8198aedd1922e1f01c69b59e62b1785e0c
jqzhao <jqzhao666@163.com>
2020-11-19 23:47:23 +0800

    last test

--- src/test/java/RobTest.java
+++ src/test/java/RobTest.java
@@ -7,7 +7,7 @@ public class RobTest {
     @Test
     public void rob() {
         Rob rob = new Rob();
-        System.out.println(rob.rob(new int[]{1,2,3,4}))   ;
+        System.out.println(rob.rob(new int[]{1,2,3,4}))  ;
 
 
     }
